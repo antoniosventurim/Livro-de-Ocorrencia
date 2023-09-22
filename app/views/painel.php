2048b26bf65a685e4b02427618da45bd6e849299
antoniosventurim <antoniosventurim@hotmail.com>
2023-09-21 23:13:34 -0300

    teste

--- app/views/painel.php
+++ app/views/painel.php
@@ -177,13 +177,13 @@ if (!empty($_GET['search'])) {
     <div class="main">
         <main class="d-flex flex-nowrap side-bar">
             <div class="d-flex flex-column flex-shrink-0 p-3 text-bg-dark" style="width: 280px;">
-                <a href="http://localhost/V3/app/views/painel" class="d-flex logo align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none">
+                <a href="http://localhost/V4/app/views/painel" class="d-flex logo align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none">
                     <span>PORTARIA DIGITAL</span>
                 </a>
                 <hr>
                 <ul class="nav nav-pills flex-column mb-auto">
                     <li class="nav-item">
-                        <a href="http://localhost/V3/app/views/painel" class="nav-link text-white" aria-current="page">
+                        <a href="http://localhost/V4/app/views/painel" class="nav-link text-white" aria-current="page">
                             <i class="bi bi-house-fill">
                                 <use xlink:href="#hom"></use>
                             </i>
