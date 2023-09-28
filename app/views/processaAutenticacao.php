ce3c8aa91b300271e553530028ea730c5a2400de
antoniosventurim <antoniosventurim@hotmail.com>
2023-09-28 20:37:02 -0300

    ajuste nos redirecionamentos

--- app/views/processaAutenticacao.php
+++ app/views/processaAutenticacao.php
@@ -7,28 +7,38 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     require_once(__DIR__ . '/../../includes/db.php');
 
     // Consulta SQL para verificar o usuário
-    $query = "SELECT id, senha, tipo_usuario, status_usuario FROM usuarios WHERE usuario = :usuario AND senha = :senha";
+    $query = "SELECT id, senha, tipo_usuario, status_usuario FROM usuarios WHERE usuario = :usuario";
     $statement = $pdo->prepare($query);
     $statement->bindParam(':usuario', $usuario);
-    $statement->bindParam(':senha', $senha);
 
     // Executar a consulta
     $statement->execute();
 
-    // Verificar se o usuário foi encontrado e está ativo (status_usuario igual a 1)
+    // Verificar se o usuário foi encontrado
     $row = $statement->fetch(PDO::FETCH_ASSOC);
+
     if ($row && $row['status_usuario'] == 1) {
-        // Obtenha o ID do usuário da consulta SQL
-        $idDoUsuario = $row['id'];
-        $tipousuario = $row['tipo_usuario'];
+        // Obtenha o hash da senha do banco de dados
+        $hashSenha = $row['senha'];
 
-        $_SESSION['usuario'] = $usuario;
-        $_SESSION['senha'] = $senha;
-        $_SESSION['id'] = $idDoUsuario; // Armazene o ID do usuário na sessão
-        $_SESSION['tipo_usuario'] = $tipousuario;
+        // Verificar a senha usando password_verify
+        if (password_verify($senha, $hashSenha)) {
+            // Senha válida
+            $idDoUsuario = $row['id'];
+            $tipousuario = $row['tipo_usuario'];
 
-        header('Location: painel.php');
-        exit;
+            $_SESSION['usuario'] = $usuario;
+            $_SESSION['senha'] = $senha;
+            $_SESSION['id'] = $idDoUsuario; // Armazene o ID do usuário na sessão
+            $_SESSION['tipo_usuario'] = $tipousuario;
+
+            header('Location: painel.php');
+            exit;
+        } else {
+            $_SESSION['mensagem'] = "Login ou senha incorretos. Tente novamente.";
+            header('Location: ../../'); // Redirecionar de volta à página de login
+            exit;
+        }
     } elseif ($row && $row['status_usuario'] == 0) {
         $_SESSION['mensagem'] = "Seu usuário está desativado. Entre em contato com o administrador.";
         header('Location: ../../'); // Redirecionar de volta à página de login
--- app/views/usuarios_cadastrados.php
+++ app/views/usuarios_cadastrados.php
@@ -139,13 +139,13 @@ $quantidadeUsuariosAtivos = $rowativos['quantidade_ativo'];
     <div class="main">
         <main class="d-flex flex-nowrap side-bar">
             <div class="d-flex flex-column flex-shrink-0 p-3 text-bg-dark menu-left">
-                <a href="http://localhost/V4/app/views/painel.php" class="d-flex logo align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none">
+                <a href="https://projetopei.dev.br/app/views/painel.php" class="d-flex logo align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none">
                     <span>PORTARIA DIGITAL</span>
                 </a>
                 <hr>
                 <ul class="nav nav-pills flex-column mb-auto">
                     <li class="nav-item">
-                        <a href="http://localhost/V4/app/views/painel.php" class="nav-link text-white" aria-current="page">
+                        <a href="https://projetopei.dev.br/app/views/painel.php" class="nav-link text-white" aria-current="page">
                             <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-house-fill" viewBox="0 0 16 16">
                                 <path d="M8.707 1.5a1 1 0 0 0-1.414 0L.646 8.146a.5.5 0 0 0 .708.708L8 2.207l6.646 6.647a.5.5 0 0 0 .708-.708L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.707 1.5Z" />
                                 <path d="m8 3.293 6 6V13.5a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 13.5V9.293l6-6Z" />
@@ -185,7 +185,7 @@ $quantidadeUsuariosAtivos = $rowativos['quantidade_ativo'];
                             <ul>
                                 <div class="collapse" id="collapseusuarios">
                                     <?php if ($tipoUsuarioLogado === 1) {
-                                        echo '<a href="#" class="nav-link text-white r-chaves" data-bs-toggle="modal" data-bs-target="#alluserss">
+                                        echo '<a href="usuarios_cadastrados.php" class="nav-link text-white r-chaves">
                                         <i class="bi bi-people-fill"></i>
                                         Usuários Registrados
                                     </a>';
